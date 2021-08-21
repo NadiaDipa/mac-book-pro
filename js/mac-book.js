563ac11aff6d0aa2a2f97e606d32a6c72b5b6d6e
NadiaDipa <nadiaislamdipa1@gmail.com>
2021-08-21 09:03:19 +0600

    added

---
+++ js/mac-book.js
@@ -0,0 +1,14 @@
+
+
+
+// // memory buttons
+// document.getElementById('memory-btn2').addEventListener('click', function () {
+//     //    const memoryCost = document.getElementById('16GB-memory');
+//     //get current price
+//     const memoryCost = document.getElementById('16GB-memory-cost');
+
+//     const memoryCostText = memoryCost.innerText;
+//     console.log(memoryCostText);7
+
+
+// })
